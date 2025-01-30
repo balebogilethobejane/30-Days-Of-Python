b52b8f8a661b4daa7e8adb1768c2b147f8f6ce21
Khosie <makhosijdlomo97@gmail.com>
2025-01-30 10:19:16 +0200

    day 3

---
+++ 03_Day_Operators/Day3/day_3.py
@@ -0,0 +1,72 @@
+# my_age = 22
+# my_height = 5.6
+# complex_number = 4 +8j
+
+# #4
+# base = int(input('base: '))
+# height = int(input('height: '))
+# area_triangle = 0.5 * base * height
+# print(f'The area of a triangle is {area_triangle}')
+
+# #5
+# a = int(input('side a: '))
+# b = int(input('side b: '))
+# c = int(input('side c: '))
+
+# perimeter = a + b + c
+# print(f'The perimeter of the triangle is: {perimeter}')
+
+# #6 
+# length = float(input('length: '))
+# width = float(input('width: '))
+# area = length * width
+# perimeter_rectangle = 2 * (length + width)
+# print(f'The area of a rectangle is: {area}')
+# print(f'The perimeter of a rectangle is: {perimeter_rectangle}')
+
+# #7
+# radius = float(input('Radius: '))
+# pi = 3.14
+# area_of_circle = pi * radius * radius
+# circumference = 2 * pi * radius
+
+# print(area_of_circle)
+# print(circumference)
+
+#8
+y = '2x - 2'
+y1 =0
+x2=0
+x_int = 2/2
+y_int = 2*(0) - 2
+
+m = (y_int - y1) / (x2 - x_int)
+print(m)
+
+#9
+x_1, y_1 = 2 ,2
+x_2, y_2 = 6, 10
+
+M = (y_2 - y_1) / (x_2 - x_1)
+print(M)
+
+#10
+print(m > M)
+print(m < M)
+print(m == M)
+print(m >= M)
+print(m <= M)
+
+#11
+y= 'x^2 +6x + 9'
+#for x values
+y1 = 0
+x1 = -3
+y_ = (-3 ** 2) + (6 * -3) + 9
+print(y_)
+
+#12
+py = len('python')
+dr = len('dragon')
+
+print(py > dr)
