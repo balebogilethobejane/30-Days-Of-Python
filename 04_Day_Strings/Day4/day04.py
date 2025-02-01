48b41da62f4eb929f9aa5403991f2e7e9b1b2148
Khosi Dlomo <makhosijdlomo97@gmail.com>
2025-02-01 14:18:28 +0200

    exercise

---
+++ 04_Day_Strings/Day4/day04.py
@@ -0,0 +1,24 @@
+#1. concatenate
+a,b,c,d = 'Thirty','Days','Of','Python'
+print(a + b +c + d)
+
+#2. Concatenate strings
+m, n, o = 'Coding', 'For', 'All'
+print(m + n + o)
+
+#3. Declaring a variable name
+company = 'Coding For All'
+#4. printing variable:
+print(company)
+#5. lenght
+print(len(company))
+#6. Uppercase 
+print(company.upper())
+#7. lowercase
+print(company.lower())
+#8. capitalize(), title(), swapcase() methods
+print(f' Capitilize: {company.capitalize()}')
+print(f'Title: {company.title()}')
+print(f' Swapcase: {company.swapcase()}')
+#9. slicing
+print(company[1:])
