98795f23a186b43141f4fb1b8b8a56b706a15829
Khosie <makhosijdlomo97@gmail.com>
2025-02-03 16:12:08 +0200

    INIT

---
+++ 05_Day_Lists/day5/day_5.py
@@ -0,0 +1,10 @@
+countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
+gr, fr, bg, sw, *rest, es = countries
+countries.insert(3, 'South Korea')
+print(countries)
+print(gr)
+print(fr)
+print(bg)
+print(sw)
+print(rest)
+print(es)
