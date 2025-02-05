66cf61debd21b2d9c50ac7edf6f78d1ffb7773c1
Lebo Thobejane <wtc@pop-os.localdomain>
2025-02-05 13:59:15 +0200

    done with excercise  1

---
+++ 02_Day_Variables_builtin_functions/variable.py
@@ -0,0 +1,34 @@
+#Day 2: 30 Days of pytho programming
+
+
+#Exercise: Level 1
+
+first_name = "Lebo"
+last_name = "Thobejane"
+full_name = "Lebogang Thobejane"
+country = "South Africa"
+city = "Pretoria "
+age = 24
+year = 2024
+is_married = False 
+is_true = True
+is_light_on = True 
+colours = "Blue" , "Green" ,"Yellow" , "Red"
+
+#Exercises: Level 2
+
+print(type(first_name)) 
+print(type(last_name))    
+print(type((full_name))) 
+print(type(country))
+print(type(city))
+print(type(age)) 
+print(type(year))    
+print(type((is_true))) 
+print(type(is_married))
+print(type(is_light_on))
+print(type(colours))
+
+print(len(first_name))
+
+print (len(first_name))
