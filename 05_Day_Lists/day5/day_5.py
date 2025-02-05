18a35152ddbbc71621cd6c06004d718993b446fe
Khosie <makhosijdlomo97@gmail.com>
2025-02-05 14:35:26 +0200

    day5  lvl 2

--- 05_Day_Lists/day5/day_5.py
+++ 05_Day_Lists/day5/day_5.py
@@ -112,3 +112,47 @@ full_stack = new_list.copy()
 full_stack.append('Python','SQL')
 print(full_stack)
 
+#Level 2 
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+#sorting a list and finding the max and min
+print(ages.sort())
+maxi = max(ages)
+mini = min(ages)
+print(maxi)
+print(mini)
+
+#adding min and max ages to a list
+sorted_list = ages.sort()
+
+#median
+median = len(sorted_list) // 2
+
+#average 
+divis = len(sorted_list)
+all_items = sum(ages)
+average = all_items / divis
+
+#range
+rang = max(ages) - min(ages)
+print(rang)
+
+#compare
+print(compare = abs(mini - average))
+print(val = abs(maxi - average))
+
+# 2. mid country
+countries = ['Afghanistan','Albania','Algeria','Andorra','Angola','Antigua and Barbuda','Argentina','Armenia','Australia','Austria','Azerbaijan',
+             'Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','Brunei','Bulgaria','Burkina Faso','Burundi',
+             'Cambodia','Cameroon','Canada','Cape Verde','Central African Republic','Chad','Chile','China','Colombi','Comoros','Congo (Brazzaville)','Congo','Costa Rica',"Cote d'Ivoire",'Croatia','Cuba','Cyprus',
+             'Czech Republic', 'Denmark','Djibouti','Dominica','Dominican Republic','East Timor (Timor Timur)','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Ethiopia','Fiji',
+             'Finland','France','Gabon','Gambia, The','Georgia','Germany','Ghana','Greece','Grenada','Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Honduras','Hungary','Iceland','India','Indonesia',
+             'Iran','Iraq','Ireland','Israel','Italy','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','Korea, North','Korea, South','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho',
+             'Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Macedonia','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Mauritania','Mauritius','Mexico','Micronesia',
+             'Moldova','Monaco','Mongolia','Morocco','Mozambique','Myanmar','Namibia','Nauru','Nepal','Netherlands','New Zealand','Nicaragua','Niger','Nigeria','Norway','Oman','Pakistan','Palau','Panama',
+             'Papua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal','Qatar','Romania','Russia','Rwanda','Saint Kitts and Nevis','Saint Lucia','Saint Vincent','Samoa','San Marino',
+             'Sao Tome and Principe','Saudi Arabia','Senegal','Serbia and Montenegro','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','Solomon Islands','Somalia','South Africa','Spain',
+             'Sri Lanka','Sudan','Suriname','Swaziland','Sweden','Switzerland','Syria','Taiwan','Tajikistan','Tanzania','Thailand','Togo','Tonga','Trinidad and Tobago', 'Tunisia','Turkey','Turkmenistan',
+             'Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States','Uruguay','Uzbekistan','Vanuatu','Vatican City','Venezuela','Vietnam', 'Yemen','Zambia','Zimbabwe']
+
+#3. half
