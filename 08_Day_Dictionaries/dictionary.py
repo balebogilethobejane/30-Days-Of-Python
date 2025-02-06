95328173504d58ba3dca4e0904c007323a143c07
Khosie <makhosijdlomo97@gmail.com>
2025-02-06 14:55:56 +0200

    init dict

--- 08_Day_Dictionaries/dictionary.py
+++ 08_Day_Dictionaries/dictionary.py
@@ -8,4 +8,13 @@ dog = {'name': 'Sam dal', 'color': 'white', 'breed': 'Husky', 'legs': 'straigh b
 print(dog)
 
 #3. student dictionary
-students = {'first_name':}
+students = {'first_name':'Khosi', 'last_name': 'Dlomo', 'gender': 'Female', 'age': 27, 'marital status':'Single', 'skills':'developing','country':'South Africa', 'city': 'JHB'}
+print(students)
+
+#4. length of the dict
+print(len(students))
+
+#5. get value and check the datatype 
+val = students.values()
+print(val)
+print(type(students.items()))
