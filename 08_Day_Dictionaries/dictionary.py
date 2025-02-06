44fa8a97a940910dec5ef8024a6c5768bd0b9cc5
Khosie <makhosijdlomo97@gmail.com>
2025-02-06 13:51:45 +0200

    dict

---
+++ 08_Day_Dictionaries/dictionary.py
@@ -0,0 +1,11 @@
+#Day 8.
+#1. empty dict 
+dog = {}
+print(dog)
+
+#2. add value, key on dict
+dog = {'name': 'Sam dal', 'color': 'white', 'breed': 'Husky', 'legs': 'straigh buffy short', 'age':2 }
+print(dog)
+
+#3. student dictionary
+students = {'first_name':}
