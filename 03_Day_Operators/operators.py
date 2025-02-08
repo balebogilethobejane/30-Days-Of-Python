a9b4b6dac5b7984774b433173de0dfe8a3d4829e
Lebogang Thobejane <balebogilethobejane@gmail.com>
2025-02-08 16:24:55 +0200

    done with exercise 3

---
+++ 03_Day_Operators/operators.py
@@ -0,0 +1,106 @@
+## 💻 Exercises - Day 3
+
+
+#1
+age = 24
+#2
+height = 154,9
+#3
+complex_number = "1 +j1"
+
+#4
+b = int(input("enter a base"))
+h = int(input ("enter a height"))
+area_of_triangle = 0,5 * b * h
+print(area_of_triangle)
+
+#5
+a = int(input("enter  side a"))
+b = int(input ("enter  side b"))
+c = int(input ("enter sibe c"))
+perimeter = a + b + c
+print(perimeter)
+
+#6
+length= int(input("enter lenghth"))
+width = int(input ("enter width"))
+area = length * width
+print(area)
+
+#7
+r = int(input("enter radius"))
+pi = 3.14
+area = pi * r *r
+circumference = 2 * pi * r
+print(area)
+print(circumference)
+
+#8
+m = 2
+x = 1
+y = -2
+x_intercept = -(-2)/2
+print(x_intercept)
+
+
+#9
+x1,y2 = 2, 2
+x2, y1 = 6,10
+slope = y2-y1 / x2 - x1
+distance = ((x2 -x1)** 2 + (y2 -y1)**2)**0.5
+print (slope)
+print(distance)
+
+#10
+slope > x_intercept
+
+#11
+Y = 0
+
+
+#12
+len_python =(len("python"))
+len_dragon = (len("dragon"))
+falsy_statement = len_python>len_dragon
+print  (not(falsy_statement))
+
+#13
+'dragon' in 'on' and 'python' in 'on'
+
+#14
+'I hope this course is not full of jargon' in 'jargon'
+
+#15
+'dragon' not in 'on' and 'python' not in 'on'
+
+#16
+len_ppython = len('python')
+num_float = float(len_ppython)
+num_string = str(num_float)
+print(num_string)
+
+#17
+n = 6
+n %2 == 0
+
+#18
+floor_division = 7 // 3 
+comparison_results = floor_division == str(2)
+ #print(floor_division)
+
+#19
+type1 = 10
+type2 = '10'
+#print (type(type1)== type(type2))
+
+#20
+num1 = '9,8'
+num2 = 10
+print(num1 == num2)
+
+#21
+hours = int(input("enter hours"))
+rate_per_hour = int(input("enter rate per hour"))
+weekly_earning = hours * rate_per_hour
+print(weekly_earning)
+
