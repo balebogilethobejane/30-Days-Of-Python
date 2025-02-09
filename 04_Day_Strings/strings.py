5164b5e459fbbf690046d25fff6750e59c897670
Lebogang Thobejane <balebogilethobejane@gmail.com>
2025-02-09 23:09:37 +0200

    started with day 4

---
+++ 04_Day_Strings/strings.py
@@ -0,0 +1,47 @@
+#Exercises - Day 4
+
+#1
+text1= "Thirty"
+text2 ="Days"
+text3 = "of"
+text4 = "Python"
+space = " "
+new_text = text1 + space + text2 +space + text3 + space + text4
+#print(new_text)
+
+#2
+text1= "Coding"
+text2 ="For"
+text3 = "All"
+space= " "
+new_text = text1 + space + text2 +space + text3 
+#print(new_text)
+
+#3
+company = "Coding For All"
+
+#4
+print(company)
+
+#5
+print(len(company))
+
+#6
+print(company.upper())
+
+#7
+print(company.lower())
+
+#8
+print(company.capitalize().title().swapcase())
+
+#9
+print(company[0:6])
+
+#10
+print("Coding" in "Coding For All")
+
+#11
+text= "Coding For All"
+new_text = (text.replace("Coding For All","python"))
+print(new_text)
