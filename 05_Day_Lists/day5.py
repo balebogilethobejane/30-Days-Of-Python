a80512aeb86e6ddf08e4d401a77cce953defa254
Lebo Thobejane <wtc@pop-os.localdomain>
2025-02-17 09:17:07 +0200

    update on day 8

--- 05_Day_Lists/day5.py
+++ 05_Day_Lists/day5.py
@@ -32,4 +32,38 @@ print(len(it_companies))
 first =(it_companies[0])
 last=(it_companies[-1])
 middle = it_companies[len(list) // 2]
-print (first ,last,middle)
+print (f'first item : {first}, last item : {last}, middle item :{middle}')
+
+#11
+it_companies.append('IT company')
+print(it_companies)
+
+#12
+it_companies.insert(3,'IT company')
+print(it_companies)
+
+#13
+company_to_leave = 'IMB'
+modified_it_companies = [company.upper() if company != company_to_leave else company for company in it_companies]
+
+print(modified_it_companies)
+
+#14
+joined_companies = '#;&nbsp; '.join(it_companies)
+
+print(joined_companies)
+
+#15
+company_to_check = 'IBM'
+if any(company == company_to_check for company in it_companies):
+    print(f"{company_to_check} exists in the list.")
+else:
+    print(f"{company_to_check} does not exist in the list.")
+
+#16
+it_companies.sort()
+print(it_companies)
+
+#17
+it_companies.sort(reverse = )
+print(it_companies)
