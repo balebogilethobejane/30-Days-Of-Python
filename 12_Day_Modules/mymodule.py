217edc51c96bd5d00ca1d6734e97e815912203c5
Khosie <makhosijdlomo97@gmail.com>
2025-02-20 11:08:33 +0200

    day1 started

--- 12_Day_Modules/mymodule.py
+++ 12_Day_Modules/mymodule.py
@@ -1,6 +1,12 @@
 #Exercises
 import random
+import string
 #Level 1.
 #1. function that generates six digit/ character random_user_id
 def random_user_id():
-    
+    id_char = string.ascii_letters + string.digits
+    user_id = ''.join(random.choice(id_char) for i in range(7)) 
+    return user_id
+print(random_user_id())
+
+#2. 
