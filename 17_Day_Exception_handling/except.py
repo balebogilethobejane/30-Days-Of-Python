9c34062b5f5cca6e1da1fa15ba8471c416606265
Khosi Dlomo <makhosijdlomo97@gmail.com>
2025-02-26 23:37:24 +0200

    nn

---
+++ 17_Day_Exception_handling/except.py
@@ -0,0 +1,13 @@
+# List of countries
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+# Unpack the first five countries into nordic_countries
+nordic_countries = names[:5]
+
+# Unpack Estonia and Russia into es and ru respectively
+es, ru = names[5], names[6]
+
+# Print the results
+print("Nordic Countries:", nordic_countries)
+print("Estonia:", es)
+print("Russia:", ru)
